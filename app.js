d15bf6ee044705b78f92a3693bc89c61c1a27e3d
JosephJamesCoop <cooper_3869@live.com>
2021-12-31 17:41:11 -0800

    generates HTML file

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
+const fs = require('fs');
+
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
 
-const printProfileData = profileDataArr => {
-    for (let i=0; i<profileDataArr.length; i++) {
-    console.log(profileDataArr[i])
-    }
-    console.log('=============');
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+//these compress into this
+// const name = profileDataArgs[0];
+// const github = profileDataArgs[1];
+const [name, github] = profileDataArgs;
 
-printProfileData(profileDataArgs);
+
+// These are the same just new writing style
+// const printProfileData = profileDataArr => {
+//     for (let i=0; i<profileDataArr.length; i++) {
+//     console.log(profileDataArr[i])
+//     }
+// };
+
+
+
+// const printProfileData = profileDataArr => {
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+
+
+
+// const fs = require('fs'); 
+// was required at the top of the page in order to make this work.
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if(err) throw new Error(err);
+    
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
